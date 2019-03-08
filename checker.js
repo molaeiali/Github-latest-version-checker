011fcf0562f7d25523acd7b2180db7af405aaba4
Ali Molaei <ali.molaei@protonmail.com>
2019-03-08 09:35:52 +0330

    Add publish date to output

--- checker.js
+++ checker.js
@@ -10,6 +10,7 @@ process.argv.forEach(function (val, index, array) {
      request('https://api.github.com/repos/'+arr[3]+'/'+arr[4]+'/releases/latest',function(error,response,body){
      let json = JSON.parse(body)
      console.log(json.tag_name === undefined? "No releases" : json.tag_name)
+     console.log(json.published_at)
      console.log('############################ '+arr[4]+' ############################')
   })}
 });
